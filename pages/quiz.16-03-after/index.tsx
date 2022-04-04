1e72b7676b47d98da4dc1e26caf7779a9aa7b947
sohyeonkim <hjthgus777@gmail.com>
2022-04-04 20:08:10 +0900

    useEffect, Lifecycle

---
+++ pages/quiz.16-03-after/index.tsx
@@ -0,0 +1,42 @@
+// 변경 후
+
+import { useRouter } from "next/router";
+import { useState, useEffect, useRef } from "react";
+
+export default function MyComponent() {
+  const router = useRouter();
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("컴포넌트가 변경됐습니다~");
+  }, [count]);
+
+  useEffect(() => {
+    console.log("컴포넌트가 마운트됐습니다~");
+    inputRef.current?.focus();
+
+    return () => {
+      console.log("컴포넌트가 제거됩니다~");
+    };
+  }, []);
+
+  const onClickCounter = () => {
+    setCount((prev) => prev + 1);
+  };
+
+  const onClickMove = () => {
+    router.push("/");
+  };
+
+  console.log("마운트 시작");
+
+  return (
+    <>
+      <input type="password" ref={inputRef} />
+      <div>카운트: {count}</div>
+      <button onClick={onClickCounter}>카운트(+1)</button>
+      <button onClick={onClickMove}>이동하기</button>
+    </>
+  );
+}
