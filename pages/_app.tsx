10e6e9a00c61297063106a91633f47341ce347e1
sohyeonkim <hjthgus777@gmail.com>
2022-04-07 18:49:41 +0900

    이미지업로드

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,25 @@
-import "antd/dist/antd.css";
 // import "../styles/globals.css"; //모든페이지에전체적으로적용하고싶은css 나중에 emotion으로 바꿀거야
+import "antd/dist/antd.css";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import Layout from "../src/components/commons/layout";
 import { Global } from "@emotion/react";
 import { globalStyles } from "../src/commons/styles/globalStyles";
+import { ApolloLink } from "@apollo/client";
+import { createUploadLink } from "apollo-upload-client";
+import { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }) {
-  //모든세팅자리
-  const client = new ApolloClient({
-    //새것을 하나 만들겠다.
-    //기능설정내용
+function MyApp({ Component, pageProps }: AppProps) {
+  //세팅 함수 부분
+  const uploadLink = createUploadLink({
     uri: "http://backend06.codebootcamp.co.kr/graphql",
+  });
+
+  const client = new ApolloClient({
+    link: ApolloLink.from([uploadLink]),
     cache: new InMemoryCache(),
   });
 
   return (
-    //아폴로기능을 제공
     <ApolloProvider client={client}>
       <Global styles={globalStyles} />
       <Layout>
